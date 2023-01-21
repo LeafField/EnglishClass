ff7d16d683fdc765f7087924d61ca48ecc4ba69c
LeafField <109466326+LeafField@users.noreply.github.com>
2023-01-21 22:42:23 +0900

    ヒーローイメージのカバースライドを製作中

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,11 @@
 import "ress";
 import "../styles/style.scss";
 
+import "swiper/swiper-bundle.min.css";
+import Swiper, { Pagination, EffectFade } from "swiper";
+Swiper.use([Pagination, EffectFade]);
+
+// ハンバーガーメニュー周りの実装
 const hamburger = <HTMLDivElement>document.querySelector(".hamburger");
 const menu = <HTMLDivElement>document.querySelector(".header__menu");
 
@@ -12,4 +17,18 @@ hamburger.addEventListener("click", () => {
 window.addEventListener("resize", () => {
   hamburger.classList.remove("active");
   menu.classList.remove("active");
+});
+
+// スライダーの実装
+new Swiper(".swiper", {
+  effect: "fade",
+  spaceBetween: 30,
+  fadeEffect: {
+    crossFade: true,
+  },
+  modules: [Pagination, EffectFade],
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
