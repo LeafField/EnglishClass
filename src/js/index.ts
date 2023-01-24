d059051c615edd3ccfc9317db4fe3c92c8b419c4
LeafField <109466326+LeafField@users.noreply.github.com>
2023-01-24 22:49:22 +0900

    courseセクションのSP版を作成

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,13 +1,22 @@
+// リセットcss
 import "ress";
+// メインのスタイル
 import "../styles/style.scss";
 
+// swiper
 import "swiper/swiper-bundle.min.css";
 import Swiper, { Pagination, EffectFade } from "swiper";
-Swiper.use([Pagination, EffectFade]);
 
-// ハンバーガーメニュー周りの実装
+/**-----------------------------------------------------------------------------------------------------
+ * ハンバーガーメニュートグルの実装
+-------------------------------------------------------------------------------------------------------- */
+
+// 必要なDOMの取得
 const hamburger = <HTMLDivElement>document.querySelector(".hamburger");
 const menu = <HTMLDivElement>document.querySelector(".header__menu");
+const links = <NodeListOf<HTMLAnchorElement>>(
+  document.querySelectorAll(".header__menu a")
+);
 
 // ハンバーガーメニューのトグル
 hamburger.addEventListener("click", () => {
@@ -28,14 +37,18 @@ const linkClickHundler = () => {
 };
 
 // linkClickHundlerをメニュー内のａタグに適用
-const links: NodeListOf<HTMLAnchorElement> =
-  document.querySelectorAll(".header__menu a");
-
 links.forEach((link) => {
   link.addEventListener("click", linkClickHundler);
 });
 
-// スライダーの実装
+/**---------------------------------------------------------------
+ * スライダーの実装
+-----------------------------------------------------------------*/
+
+// 必要なモジュールの登録
+Swiper.use([Pagination, EffectFade]);
+
+// swiperのイニシャライズ
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   fadeEffect: {
@@ -51,3 +64,34 @@ const swiper = new Swiper(".swiper", {
 
 // クリックしたら次のスライドへ
 swiper.on("click", () => [swiper.slideNext()]);
+
+/**-----------------------------------------------------------
+ * タブパネルの動作部分の実装
+--------------------------------------------------------------*/
+
+// タブの取得
+const allTabs = <NodeListOf<HTMLDivElement>>(
+  document.querySelectorAll(".course__tab--container")
+);
+
+// パネルの取得
+const allPanels = <NodeListOf<HTMLDivElement>>(
+  document.querySelectorAll(".course__panel")
+);
+
+// クリックしたタブとパネルの連動
+
+// 各タブとパネルから一度activeクラスを取り除き、クリックしたタブとそれに対応するパネルにactiveクラスを付与
+const tabHundler = (index: number) => {
+  allTabs.forEach((tab) => tab.classList.remove("active"));
+  allTabs[index].classList.add("active");
+  allPanels.forEach((panel) => panel.classList.remove("active"));
+  allPanels[index].classList.add("active");
+};
+
+// 各タブとパネルを連動
+allTabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabHundler(index);
+  });
+});
